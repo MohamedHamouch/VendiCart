7f4ef8e68397ad172538d12ec26735698b18a528
MohamedHamouch <med10hamouch@gmail.com>
2025-03-17 11:33:51 +0000

    fix migration for product table

--- database/migrations/2025_03_11_150023_create_products_table.php
+++ database/migrations/2025_03_11_150023_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
-            $table->integer('stock')->default(0);
+            $table->integer('stock_quantity')->default(0);
             $table->string('image')->nullable();
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->boolean('is_active')->default(true);
